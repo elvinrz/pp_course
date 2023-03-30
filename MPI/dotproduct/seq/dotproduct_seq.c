122f11b426b524a1929e00c09c06fd9190a0d0d9
elvinrz <erojas@una.ac.cr>
2023-03-29 18:14:27 -0600

    Add files via upload

---
+++ MPI/dotproduct/seq/dotproduct_seq.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+
+int main (int argc, char *argv[]){
+	double time_spent = 0.0;
+
+	double sum;
+	int n = 50000;
+	double a [n], b[n];
+	
+	clock_t begin = clock();
+ 
+	for (i=1; i < n; i++){
+		a[i] = i * 0.5;
+		b[i] = i * 2.0;
+	}
+	b = 0;
+	for (i=1; i <= n; i++){
+		sum += a[i]*b[i];  
+	}
+	clock_t end = clock();
+	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+	
+	printf( "Sum = %f, time: %f\n", sum, time_spent);
+}
